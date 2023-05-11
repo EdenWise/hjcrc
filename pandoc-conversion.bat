b36b62125a1c32868dea458e35610be67093fd49
Todd Partridge <toddpartridge@outlook.com>
2023-05-11 13:49:25 -0700

    Tidy up, echo off, wrapping turn off.

--- pandoc-conversion.bat
+++ pandoc-conversion.bat
@@ -1,1 +1,8 @@
-pandoc.exe -f markdown -t docx --reference-doc=reference.docx .\hjcrc_draft.md -o .\hjcrc_draft.docx
+:: Text convert from Markdown to Microsoft Word
+
+:: Print everything: off
+@echo off
+
+:: style.docx:  allowed styles to import (File>Options>Ribbon>Developer;Dev>DocTemplt>Organizer...)
+:: --wrap=none: wrapping turn off (72 characters by default)
+pandoc.exe -f markdown -t docx --reference-doc=style.docx --wrap=none .\hjcrc_draft.md -o .\hjcrc_draft.docx
