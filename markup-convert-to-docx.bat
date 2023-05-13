3af3e7aa4d7bfed1e432a8c011538f852cf15d6f
Todd Partridge <toddpartridge@outlook.com>
2023-05-13 15:22:30 -0700

    All files as hjcrc.{md,docx}

--- markup-convert-to-docx.bat
+++ markup-convert-to-docx.bat
@@ -8,4 +8,4 @@
 :: .\pandoc.exe -f docx -t markdown --preserve-tabs --wrap=none .\hjcrc.docx -o .\hjcrc.md
 :: style.docx:  allowed styles to import (File>Options>Ribbon>Developer;Dev>DocTemplt>Organizer...)
 :: --wrap=none: wrapping turn off (72 characters by default)
-pandoc.exe -f markdown -t docx --reference-doc=style.docx --wrap=none .\hjcrc_draft.md -o .\hjcrc_draft.docx
+pandoc.exe -f markdown -t docx --reference-doc=style.docx --wrap=none .\hjcrc.md -o .\hjcrc.docx
