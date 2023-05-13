d8870679ae7835dff1d3e380910d0ecf77473301
Todd Partridge <toddpartridge@outlook.com>
2023-05-13 13:52:00 -0700

    docx to markdown command updated.

--- markup-convert-to-docx.bat
+++ markup-convert-to-docx.bat
@@ -1,11 +1,11 @@
-:: Text convert from Markdown to Microsoft Word
+:: Text convert (Markdown to Microsoft Word).
 
 :: Print everything: off
 @echo off
 
-:: MS Word convert to:
+:: FYI: Text convert (Microsoft Word to Markdown)
 :: FAILS: tabs still being converted to newlines: --preserve-
-:: pandoc.exe -f docx -t markdown --preserve-tabs --wrap=none .\hjcrc_draft.docx -o .\hjcrc_draft.md
+:: .\pandoc.exe -f docx -t markdown --preserve-tabs --wrap=none .\hjcrc_draft.docx -o .\hjcrc.md
 :: style.docx:  allowed styles to import (File>Options>Ribbon>Developer;Dev>DocTemplt>Organizer...)
 :: --wrap=none: wrapping turn off (72 characters by default)
 pandoc.exe -f markdown -t docx --reference-doc=style.docx --wrap=none .\hjcrc_draft.md -o .\hjcrc_draft.docx
