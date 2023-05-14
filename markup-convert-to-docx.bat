48ae8cab83971d556e192032518f67fac91ea9f2
Todd Partridge <toddpartridge@outlook.com>
2023-05-14 12:35:23 -0700

    style.docx to template.docx

--- markup-convert-to-docx.bat
+++ markup-convert-to-docx.bat
@@ -8,4 +8,4 @@
 :: .\pandoc.exe -f docx -t markdown --preserve-tabs --wrap=none .\hjcrc.docx -o .\hjcrc.md
 :: style.docx:  allowed styles to import (File>Options>Ribbon>Developer;Dev>DocTemplt>Organizer...)
 :: --wrap=none: wrapping turn off (72 characters by default)
-pandoc.exe -f markdown -t docx --reference-doc=style.docx --wrap=none .\hjcrc.md -o .\hjcrc.docx
+pandoc.exe .\hjcrc.md -f markdown -t docx --reference-doc=template.docx --wrap=none -o .\hjcrc.docx
