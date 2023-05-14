de402bb27ca0fa8820ff9619ba64625c673b5b64
Todd Partridge <toddpartridge@outlook.com>
2023-05-14 15:24:09 -0700

    Attempting --variable=indent

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -1,16 +1,19 @@
 :: Conversion: Markdown to Microsoft-Word.
 
-:: Print everything verbatim: off
+:: Print everything: off
 @echo off
 
-:::: FYI: Conversion: Microsoft-Word to Markdown.
-::::  .\pandoc.exe --from docx --to markdown --preserve-tabs --wrap=none .\hjcrc.docx -o .\hjcrc.md
-:::: Paragraph with indents: --preserve-tabs              FAIL: Zero tabs produced? Docx has paragraphs with indents, only includes indents? and zero tabs in markdown.
-
-:: --variable indent=
+:: Blank lines convert to paragraphs with indents
+:: FAIL: docx+empty_paragraphs      : paragraph with indents---erroneous tip
+:: --variable=indent
+:: oops?!: --variable indent=
 :: markdown to docx convert blank lines to paragraph
 
-:: style.docx:  allowed styles to import (File>Options>Ribbon>Developer;Dev>DocTemplt>Organizer...)
-:: --wrap=none: wrapping turn off (72 characters by default)
-pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --wrap=none -o .\hjcrc.docx
-:: Paragraph with indents: --to docx+empty_paragraphs   FAIL: Erroroneos tip
+pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --wrap=none --variable=indent -o .\hjcrc.docx
+:: --reference-doc=template.docx    : template of styles (allowed) to import
+::                                  : (File > Options > Ribbon > Developer;Dev > DocTemplt > Organizer...)
+:: --wrap=none                      : editor should wrap lines
+
+:::: Conversion: Microsoft-Word to Markdown.
+:::: .\pandoc.exe --from docx --to markdown --wrap=none .\hjcrc.docx -o .\hjcrc.md
+::::    paragraph with indents: --preserve-tabs   FAILS: docx is paragraphs with indents.
