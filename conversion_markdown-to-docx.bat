b69ebc7b2c0865dcb22f3cf04d1e8c2f529f9330
Todd Partridge <toddpartridge@outlook.com>
2023-05-14 13:50:01 -0700

    Script cleanup.

---
+++ conversion_markdown-to-docx.bat
@@ -0,0 +1,13 @@
+:: Conversion: Markdown to Microsoft-Word.
+
+:: Print everything: off
+@echo off
+
+:::: FYI: Conversion: Microsoft-Word to Markdown.
+::::  .\pandoc.exe --from docx --to markdown --preserve-tabs --wrap=none .\hjcrc.docx -o .\hjcrc.md
+:::: Paragraph with indents: --preserve-tabs              FAIL: Zero tabs produced? Docx has paragraphs with indents, only includes indents? and zero tabs in markdown.
+
+:: style.docx:  allowed styles to import (File>Options>Ribbon>Developer;Dev>DocTemplt>Organizer...)
+:: --wrap=none: wrapping turn off (72 characters by default)
+pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --wrap=none -o .\hjcrc.docx
+:: Paragraph with indents: --to docx+empty_paragraphs   FAIL: Erroroneos tip
