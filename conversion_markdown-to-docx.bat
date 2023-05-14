1fb5b6e07167d10eeecfcbb3f44235335f61a040
Todd Partridge <toddpartridge@outlook.com>
2023-05-14 16:22:48 -0700

    Clean up, added template building section.

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -3,21 +3,28 @@
 :: Print everything: off
 @echo off
 
-
-:: Blank lines convert to paragraphs with indents
-:: FAIL: docx+empty_paragraphs      : paragraph with indents---erroneous tip
-
-:: CAN ONLY BE USED IN TEMPLATES!!!!!!
-:: markdown to docx convert blank lines to paragraph
-:::: F: --variable=indent
-:::: F: --variable indent=2
-:::: F: --variable=indent2
-
-pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --wrap=none -o .\hjcrc.docx
-:: --reference-doc=template.docx    : template of styles (allowed) to import
-::                                  : (File > Options > Ribbon > Developer;Dev > DocTemplt > Organizer...)
-:: --wrap=none                      : editor should wrap lines
-
 :::: Conversion: Microsoft-Word to Markdown.
 :::: .\pandoc.exe --from docx --to markdown --wrap=none .\hjcrc.docx -o .\hjcrc.md
-::::    paragraph with indents: --preserve-tabs   FAILS: docx is paragraphs with indents.
+::::      --wrap=none                       : editor should wrap lines
+
+:::: Template building (original style of hjcrc.docx import to template)
+::::    > Generate allowable style template:
+::::      pandoc --print-default-data-file reference.docx --variable indent2 -o template.docx 
+::::    > Word-and-hjcrc.docx: File > Options > Ribbon > +Developer > Dev Tab > DocTemplt > 
+::::        Organizer > Copy allowable styles to right and save
+::::    > Word-and-tmple.docx: Organizer > copy styles to left.
+:::: F: --variable=indent
+:::: F: --variable indent=2
+:::: F: --variable indent2
+
+pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --wrap=none -o .\hjcrc.docx
+:: --reference-doc=template.docx    : template of styles (allowed to import), see Template building.
+::                                  : 
+:: --wrap=none                      : editor should wrap lines
+
+:: docx with paragraph indents
+:: FAIL :: docx-md :: --preserve-tabs                   : fails because docx is paragraphs with indents.
+:: FAIL :: docx+empty_paragraphs                        : erroneous tip?
+:: FAIL :: md-docx :: --preserve-tabs                   : fails because tabs are different then indents?
+:: Fixes blank lines?
+:: [General writer}(https://pandoc.org/MANUAL.html#general-writer-options-1) options may help.
