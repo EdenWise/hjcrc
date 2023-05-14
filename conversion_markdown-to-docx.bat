cf132140f3fd1d76ac132377279d900e7b7f8a2a
Todd Partridge <toddpartridge@outlook.com>
2023-05-14 15:45:37 -0700

    Removed --variable from conversion (only for template)

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -3,14 +3,17 @@
 :: Print everything: off
 @echo off
 
+
 :: Blank lines convert to paragraphs with indents
 :: FAIL: docx+empty_paragraphs      : paragraph with indents---erroneous tip
+
+:: CAN ONLY BE USED IN TEMPLATES!!!!!!
 :: markdown to docx convert blank lines to paragraph
 :::: F: --variable=indent
 :::: F: --variable indent=2
-:::: --variable=indent2
+:::: F: --variable=indent2
 
-pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --wrap=none --variable=indent2 -o .\hjcrc.docx
+pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --wrap=none -o .\hjcrc.docx
 :: --reference-doc=template.docx    : template of styles (allowed) to import
 ::                                  : (File > Options > Ribbon > Developer;Dev > DocTemplt > Organizer...)
 :: --wrap=none                      : editor should wrap lines
