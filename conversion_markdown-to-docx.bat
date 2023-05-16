984f5c012e827ec8be50d11ad9d60b6f5ae436d1
Todd Partridge <toddpartridge@outlook.com>
2023-05-16 15:07:27 -0700

    Added: Word tasks to do for hjcrc.docx:

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -21,9 +21,8 @@
 ::::      > Word-and-tmple.docx: Organizer > copy styles to left.
 
 .\pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
-:: FAIL:  --variable=indent:2m    .\pandoc.exe .\hjcrc.md --from markdown --to docx --reference-doc=template.docx --variable=indent:2m --wrap=none --output .\hjcrc.docx
 ::        --wrap=none                      : editor should wrap lines
-::        svgs require librsvg. Converts to png!? Something like?
+::        svgs require librsvg. Converts to png!? Something like?:
 ::            rsvg-convert --format=png --dpi-x=220 --dpi-y=220 --output <name>.png <name>.svg
 
 :: docx with paragraph indents
@@ -32,3 +31,7 @@
 :: FAIL :: md-docx :: --preserve-tabs                   : fails because tabs are different then indents?
 :: Fixes blank lines?
 :: [General writer}(https://pandoc.org/MANUAL.html#general-writer-options-1) options may help.
+
+:: Word tasks to do for hjcrc.docx:
+:: pictures:    text-wrap:tight, align:right, text-only:left
+:: paragraphs:  
