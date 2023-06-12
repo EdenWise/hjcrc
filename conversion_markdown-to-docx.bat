f7c92593ae7ef53dba20689e9b3b2055a32ea889
Todd Partridge <toddpartridge@outlook.com>
2023-06-12 13:44:05 -0700

    .

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -6,6 +6,9 @@
 :: Path: Add executables to path.
 set PATH=D:\;%PATH%
 
+:: Personal: Add pandoc.exe to path of executables:
+echo "`$env:Path += `"D:;`""
+
 pandoc.exe .\hjcrc.md --from markdown+inline_notes --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
 ::        --wrap=none                      : editor should wrap lines
 ::        svgs require librsvg. Converts to png!? Something like?:
