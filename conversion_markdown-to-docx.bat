32fc74f4a39a7e3ecd195ab502017dce478361d5
Todd Partridge <toddpartridge@outlook.com>
2023-06-12 18:32:25 -0700

    .

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -1,11 +1,12 @@
 :: About: Text convert from Markdown to Microsoft-Word.
 
-:: Path: Add executables to path.
-set PATH=D:\;%PATH%
 :: Personal: Print command to add my pandoc.exe to path of executables:
 echo "`$env:Path += `"D:;`""
 
 :: Print everything: off
 @echo off
+
+:: Path: Add executables to path.
+set PATH=D:\;%PATH%
 
 pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
