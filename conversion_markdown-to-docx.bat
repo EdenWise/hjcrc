0e22b83a85107e59e21c7da8038a21d460a5736c
Todd Partridge <toddpartridge@outlook.com>
2023-06-12 18:28:55 -0700

    .

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -2,11 +2,10 @@
 
 :: Path: Add executables to path.
 set PATH=D:\;%PATH%
+:: Personal: Print command to add my pandoc.exe to path of executables:
+echo "`$env:Path += `"D:;`""
 
 :: Print everything: off
 @echo off
-
-:: Personal: Add pandoc.exe to path of executables:
-echo "`$env:Path += `"D:;`""
 
 pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
