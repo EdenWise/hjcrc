4850849ba60180d25c3d8dc6f44cb1fe50e92995
Todd Partridge <toddpartridge@outlook.com>
2023-10-30 11:07:45 -0700

    Update conversion_markdown-to-docx.bat

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -7,4 +7,7 @@
 set PATH=D:\;%PATH%
 set PATH=E:\;%PATH%
 
+set mypath=%cd%
+https://stackoverflow.com/questions/17063947/get-current-batchfile-directory
+
 pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
