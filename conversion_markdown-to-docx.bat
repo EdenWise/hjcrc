649a2e238b6a125c6cb00c0cfd506196070b634c
Todd Partridge <toddpartridge@outlook.com>
2023-10-30 11:09:03 -0700

    Update conversion_markdown-to-docx.bat

--- conversion_markdown-to-docx.bat
+++ conversion_markdown-to-docx.bat
@@ -6,8 +6,9 @@
 :: Path: Add executables to path.
 set PATH=D:\;%PATH%
 set PATH=E:\;%PATH%
+set batdir=%~dp0
 
-set mypath=%cd%
-https://stackoverflow.com/questions/17063947/get-current-batchfile-directory
+:: set mypath=%cd%
+:: https://stackoverflow.com/questions/17063947/get-current-batchfile-directory
 
 pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
