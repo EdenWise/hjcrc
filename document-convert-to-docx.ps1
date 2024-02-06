ca4442c5e5be851205fc607783bc3c7523772023
Todd Partridge <toddpartridge@outlook.com>
2024-02-06 16:31:28 -0700

    .

--- document-convert-to-docx.ps1
+++ document-convert-to-docx.ps1
@@ -4,7 +4,7 @@
 # $PATH_OF_FILE = "($PSSCRIPTROOT | Split-Path -Qualifier)\Documents"
 
 ## FONT CONSTANIA
-# pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
+# pandoc.exe .\HJCSIC.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
 
 ## FONT TIMES NEW ROMAN
-pandoc .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template_font-times.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
+pandoc .\HJCSIC.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template_font-times.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
