479fb43bb01b775178f0baa209bc9be7bd5faa83
Todd Partridge <toddpartridge@outlook.com>
2024-02-06 15:23:13 -0700

    .

--- document-convert-to-docx.ps1
+++ document-convert-to-docx.ps1
@@ -1,3 +1,10 @@
+## DOCUMENT CONVERT FROM MARKDOWN TO DOCX
+#
+# $PATH_OF_FILE = "${env:USERPROFILE}\Documents\HJCSIC.docx"
+# $PATH_OF_FILE = "($PSSCRIPTROOT | Split-Path -Qualifier)\Documents"
 
-# pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
-pandoc .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template_font-times.docx --dpi=220 --wrap=none --output .\hjcrc.docx
+## FONT CONSTANIA
+# pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
+
+## FONT TIMES NEW ROMAN
+pandoc .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template_font-times.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
