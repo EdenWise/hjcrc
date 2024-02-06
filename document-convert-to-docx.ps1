85ff88623aec5fbfc522692aff1f03096857d198
Todd Partridge <toddpartridge@outlook.com>
2024-02-05 17:13:34 -0700

    .

---
+++ document-convert-to-docx.ps1
@@ -0,0 +1,3 @@
+
+# pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
+pandoc .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template_font-times.docx --dpi=220 --wrap=none --output .\hjcrc.docx
