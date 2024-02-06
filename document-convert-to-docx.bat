56af2729d2dcc482317cde0a56d6fdf01577508a
Todd Partridge <toddpartridge@outlook.com>
2024-02-05 17:03:57 -0700

    .

--- document-convert-to-docx.bat
+++ document-convert-to-docx.bat
@@ -12,4 +12,5 @@
 :: set mypath=%cd%
 :: https://stackoverflow.com/questions/17063947/get-current-batchfile-directory
 
-pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
+::pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\hjcrc.docx
+pandoc.exe .\hjcrc.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template_font-times.docx --dpi=220 --wrap=none --output .\hjcrc.docx
