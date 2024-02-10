c622181f47bf9956ac1fa293bf4b7f55a4a4f5e3
Todd Partridge <toddpartridge@outlook.com>
2024-02-10 15:38:06 -0700

    .

--- document-convert-to-docx.ps1
+++ document-convert-to-docx.ps1
@@ -1,10 +1,16 @@
 ## DOCUMENT CONVERT FROM MARKDOWN TO DOCX
 #
-# $PATH_OF_FILE = "${env:USERPROFILE}\Documents\HJCSIC.docx"
-# $PATH_OF_FILE = "($PSSCRIPTROOT | Split-Path -Qualifier)\Documents"
+## NOTES
+  # Fonts tried by order of preference: Constantia, Noto Serif
+    # EB Garramond, Georgia, Lora
+  # $PATH_OF_FILE = "${env:USERPROFILE}\Documents\HJCSIC.docx"
+  # $PATH_OF_FILE = "($PSSCRIPTROOT | Split-Path -Qualifier)\Documents"
 
-## FONT CONSTANIA
-# pandoc.exe .\HJCSIC.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
-
-## FONT TIMES NEW ROMAN
-pandoc .\HJCSIC.md --from markdown+inline_notes+escaped_line_breaks --to docx --reference-doc=template_font-times.docx --dpi=220 --wrap=none --output .\HJCSIC.docx
+## CONVERT
+pandoc .\HJCSIC.md `
+       --from markdown+inline_notes+escaped_line_breaks `
+       --to docx `
+       --reference-doc=template_font-times.docx `
+       --dpi=220 `
+       --wrap=none `
+       --output .\HJCSIC.docx
